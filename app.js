3b516be728192834a2dd5d92365b3f876645de10
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2023-10-12 10:57:12 +0900

    응답 처리

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-
+    res.writeHead (200, {"Content-Type":"text/plain"})
+    res.write('향마루');
+    res.end();
 });
 
 server.listen(1111,(err)=>{
@@ -10,4 +12,4 @@ server.listen(1111,(err)=>{
     } else {
         console.log("1111 포트에 서버 열림 http://localhost:1111");
     }
-})
+});
